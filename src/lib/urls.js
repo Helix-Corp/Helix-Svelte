0683f11d3b29d401268b163fae6a8ed827fc0313
Connor <connorburkeofficial@gmail.com>
2022-08-29 15:26:53 -0700

    modified to be more working

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -2,6 +2,6 @@
  * @file Server-related URL constants.
  */
 
-export const linkUrl = localStorage.getItem("meower_linkurl") || "wss://server.meower.org/";
-export const apiUrl = localStorage.getItem("meower_apiurl") || "https://api.meower.org/";
+export const linkUrl = localStorage.getItem("meower_linkurl") || "wss://server.helixia.tk/";
+export const apiUrl = localStorage.getItem("meower_apiurl") || "https://api.helixia.tk/";
 export const encodeApiURLParams = localStorage.getItem("meower_encodeapi") || false;
